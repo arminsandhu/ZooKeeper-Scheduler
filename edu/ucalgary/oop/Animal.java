98a3ba33b1edf3a9d03ec74c3cc837bb8ff14c4d
Dominico <Coco@Dominicos-MacBook-Pro.local>
2023-03-29 20:28:32 -0600

    Adgin layout code for some files

--- edu/ucalgary/oop/Animal.java
+++ edu/ucalgary/oop/Animal.java
@@ -1,5 +1,33 @@
 package edu.ucalgary.oop;
 
 public class Animal {
-    
+    private final int ANIMALID;
+    private final String ANIMALSPECIES;
+    private Task assignedTask;
+    private final String ANIMALNICKNAME;
+
+
+    public Animal(int id, String name, String species, Task task) {
+        // ADD STUFF
+    }
+
+    public void setAssignedTask(Task task) {
+        this.assignedTask = task;
+    }
+
+    public String getAnimalSpecies() {
+        return this.ANIMALSPECIES;
+    }
+
+    public Task getAssignedTask() {
+        return this.assignedTask;
+    }
+
+    public int getAnimalID() {
+        return this.ANIMALID;
+    }
+
+    public String getNickname() {
+        return this.ANIMALNICKNAME;
+    }
 }
